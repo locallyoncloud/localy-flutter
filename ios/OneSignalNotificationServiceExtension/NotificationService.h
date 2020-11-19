7d8191bbe46c127438594be2d307733c240b0958
Levent Özkan <leventozkan89@gmail.com>
2020-11-20 02:15:07 +0300

    no message

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Levent Özkan on 19.11.2020.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
